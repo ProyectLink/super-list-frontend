7fa4741b577443ab1dbac6ec445081e92086ae96
Bryan Lundberg <bryanlundberg@outlook.com>
2024-08-28 10:08:40 -0600

    refactor router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,21 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import Login from "@/pages/Login/Login";
 import TasksPage from "@/pages/Tasks/TasksPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "all-tasks",
+    element: <TasksPage />,
+  },
+]);
+
 const Router = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/all-tasks" element={<TasksPage />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 };
