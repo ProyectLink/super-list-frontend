fdac4d00d99a7db3e234237b523d57fa310dad8e
Bryan Lundberg <bryanlundberg@outlook.com>
2024-08-29 17:55:20 -0600

    feat: no found page

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import Layout from "@/components/layouts/Layout";
 import SubTask from "@/pages/Tasks/SubTask";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
+import NotFound from "./pages/Not-Found/NotFound";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
