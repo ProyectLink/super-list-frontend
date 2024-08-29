ba88aa3470bc22c310d32115596bd3e4433d3bf6
Bryan Lundberg <bryanlundberg@outlook.com>
2024-08-29 08:38:06 -0600

    refactor: router inside App (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Router from "./router/router"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const  App = () =>  {
+import Login from "@/pages/Login/Login";
+import Tasks from "@/components/tasks/Tasks";
+import Layout from "@/components/layouts/Layout";
+import SubTask from "@/pages/Tasks/SubTask";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+
+const App = () => {
   return (
     <>
-     <Router/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="all-tasks" element={<Layout main={<Tasks />} />}>
+            <Route path=":taskId" element={<SubTask />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
