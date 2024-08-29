81ecf787fffb57f384d23ef52e5da57777d47cfb
Bryan Lundberg <bryanlundberg@outlook.com>
2024-08-29 10:09:32 -0600

    initial layout home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -28,6 +28,61 @@ const Home = () => {
             </div>
           </div>
         </div>
+
+        {/* title */}
+        <div className="flex flex-col items-center mx-auto mt-20 text-5xl font-bold md:text-7xl text-neutral-800">
+          <p>Home to</p>
+          <div className="flex">
+            <p>all your </p>
+            <img src="/check.png" className="mx-2 my-auto size-11 md:size-16" />
+            <p className="text-[#F74F39]">lists</p>
+          </div>
+
+          {/* message big screen */}
+          <div className="hidden mt-5 text-xl font-semibold text-center opacity-60 md:block">
+            <p>Take notes, organize your work, and get more done with AI.</p>
+            <p>Simple, blazingly fast, and wrapped in a beautiful UI.</p>
+          </div>
+
+          {/* message small screens */}
+          <div className="mt-5 text-xl font-semibold text-center opacity-60 md:hidden">
+            <p>For team work, personal projects,</p>
+            <p>and everything in between.</p>
+          </div>
+
+          <div className="flex gap-3 mt-5 text-lg font-bold">
+            <button className="py-2 bg-[#F84F39] text-white rounded-full px-3">
+              Start today — it's free{" "}
+            </button>
+            <button className="py-2 bg-[#6B66DA] text-white rounded-full px-3">
+              Go Pro ✨
+            </button>
+          </div>
+        </div>
+
+        <div className="px-5">
+          {/* app sample */}
+          <div className="grid w-full grid-cols-4 gap-3 p-3 mx-auto mt-10 border size-96 rounded-2xl border-[#EAE9F3] max-w-[1250px]">
+            {/* col 1 */}
+            <div className="w-full h-full col-span-1 bg-white rounded-2xl">
+              {/* rectangles */}
+              <div className="grid grid-cols-2 gap-2 p-3">
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-14"></div>
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-14"></div>
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-14"></div>
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-14"></div>
+              </div>
+
+              <div className="font-semibold text-gray-500 ms-3">List</div>
+            </div>
+
+            {/* col mid */}
+            <div className="w-full h-full col-span-2 bg-white rounded-2xl"></div>
+
+            {/* col 3 */}
+            <div className="w-full h-full col-span-1 bg-white rounded-2xl"></div>
+          </div>
+        </div>
       </div>
     </>
   );
