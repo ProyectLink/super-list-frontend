d285c9a6611cfc5b854fc7a6af7ea398b71b4b34
Bryan Lundberg <bryanlundberg@outlook.com>
2024-08-29 17:06:48 -0600

    add missing elements on navbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,14 @@ import { ArrowRight, CheckIcon } from "lucide-react";
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col min-h-screen border-t-[1rem] border-[#F74F39] bg-[#F4F3FA] text-foreground">
+      <div className="flex flex-col min-h-screen border-t-[1rem] md:border-t-[0.3rem] border-[#F74F39] bg-[#F4F3FA]">
         {/* navbar */}
         <div className="flex items-center justify-between px-10 pt-10">
           {/* left options */}
           <div className="flex items-center gap-3 font-semibold">
             <img src="/icon.png" className="size-5" />
             <img src="/logo.svg" className="h-5 mt-auto" />
+            <div className="hover:opacity-60 hover:cursor-pointer">MacOS</div>
             <div className="hover:opacity-60 hover:cursor-pointer">Web</div>
             <div className="hover:opacity-60 hover:cursor-pointer">iOS</div>
             <div className="hover:opacity-60 hover:cursor-pointer">Android</div>
@@ -18,6 +19,7 @@ const Home = () => {
           {/* right options */}
           <div className="flex font-semibold gap-9">
             <div className="hover:opacity-60 hover:cursor-pointer">Updates</div>
+            <div className="hover:opacity-60 hover:cursor-pointer">Pricing</div>
             <div className="text-[#F74F39] hover:cursor-pointer">
               Sign in{" "}
               <span>
