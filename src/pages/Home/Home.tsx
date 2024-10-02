ad85631e57f6a95ba08743cf64b7773df732f771
Ankit Kumar <139707943+Kumar-Ankit07@users.noreply.github.com>
2024-10-02 12:08:39 +0530

    Added Dark Mode in the home nav bar section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -104,22 +104,22 @@ const Home = () => {
                 </NavigationItem>
                 <NavigationItem active={false}>
                   <p className="py-2 font-semibold text-gray-800 hover:bg-[#F3F3F7]">
-                    <span className="me-3">🏘️</span>Kitchen Reno
+                    <span className="me-3">🏘️</span> Kitchen Reno
                   </p>
                 </NavigationItem>
                 <NavigationItem active={false}>
                   <p className="py-2 font-semibold text-gray-800 hover:bg-[#F3F3F7]">
-                    <span className="me-3">🧘</span>Daily Habits
+                    <span className="me-3">🧘</span> Daily Habits
                   </p>
                 </NavigationItem>
                 <NavigationItem active={false}>
                   <p className="py-2 font-semibold text-gray-800 hover:bg-[#F3F3F7]">
-                    <span className="me-3">🍔</span>Recipes
+                    <span className="me-3">🍔</span> Recipes
                   </p>
                 </NavigationItem>
                 <NavigationItem active={false}>
                   <p className="py-2 font-semibold text-gray-800 hover:bg-[#F3F3F7]">
-                    <span className="me-3">✏️</span>Design Work
+                    <span className="me-3">✏️</span> Design Work
                   </p>
                 </NavigationItem>
               </div>
@@ -161,166 +161,45 @@ const Home = () => {
                   />
                   <img
                     className="inline-block w-10 h-10 rounded-full ring-2 ring-white z-[2]"
-                    src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                  <img
+                    className="inline-block w-10 h-10 rounded-full ring-2 ring-white z-[1]"
+                    src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     alt=""
                   />
                 </div>
-                <div className="font-bold text-gray-500">+6</div>
+
+                <h2 className="text-xl font-bold text-gray-800">App Launch</h2>
               </div>
-              <p className="mt-8 text-4xl font-bold opacity-85">App Launch</p>
-              <p className="w-10/12 mt-5 text-lg font-semibold">
-                Hey team, here's a quick recap of what we're working on ahead of
-                the launch. Feel free to add any more notes or details to the
-                tasks.
+
+              <p className="mt-10 font-semibold text-neutral-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+                dolorum!
               </p>
-
-              {/* activity list container */}
-              <div className="mt-8 space-y-6">
-                {/* activity item (user) */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="bg-[#F84F39] rounded-lg size-6 flex items-center justify-center">
-                    <CheckIcon size={20} className="text-white -rotate-12" />
-                  </div>
-
-                  <div className="text-xl line-through decoration-orange-500 decoration-2 text-neutral-400 grow">
-                    Launch readiness
-                  </div>
-
-                  <img
-                    className="rounded-full size-7"
-                    src="/profile/photo-1517365830460-955ce3ccd263.avif"
-                  />
-                </div>
-
-                {/* activity item (google - user) */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="bg-[#F84F39] rounded-lg size-6 flex items-center justify-center">
-                    <CheckIcon size={20} className="text-white -rotate-12" />
-                  </div>
-
-                  <div className="text-xl line-through decoration-orange-500 decoration-2 text-neutral-400 grow">
-                    Value proposition
-                  </div>
-
-                  <div className="flex gap-3">
-                    <img className="rounded-full size-7" src="/google.webp" />
-                    <img
-                      className="rounded-full size-7"
-                      src="/profile/photo-1500648767791-00dcc994a43e.avif"
-                    />
-                  </div>
-                </div>
-
-                {/* activity item (google - user) off */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="flex items-center justify-center border-2 rounded-lg size-6 group/checkbox border-neutral-400 text-neutral-400">
-                    <CheckIcon
-                      size={18}
-                      className="invisible group-hover/checkbox:visible -rotate-12"
-                    />
-                  </div>
-
-                  <div className="text-xl text-black decoration-2 grow">
-                    Marketing strategy
-                  </div>
-
-                  <div className="flex gap-3">
-                    <img
-                      className="rounded-full size-7"
-                      src="/profile/photo-1550525811-e5869dd03032.avif"
-                    />
-                  </div>
-                </div>
-
-                {/* activity item (linear - user) off */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="flex items-center justify-center border-2 rounded-lg size-6 group/checkbox border-neutral-400 text-neutral-400">
-                    <CheckIcon
-                      size={18}
-                      className="invisible group-hover/checkbox:visible -rotate-12"
-                    />
-                  </div>
-
-                  <div className="text-xl text-black decoration-2 grow">
-                    Design system update
-                  </div>
-
-                  <div className="flex gap-3">
-                    <img className="rounded-full size-7" src="/linear.webp" />
-                    <img
-                      className="rounded-full size-7"
-                      src="/profile/photo-1491528323818-fdd1faba62cc.avif"
-                    />
-                  </div>
-                </div>
-
-                {/* activity item (google - user) off */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="flex items-center justify-center border-2 rounded-lg size-6 group/checkbox border-neutral-400 text-neutral-400">
-                    <CheckIcon
-                      size={18}
-                      className="invisible group-hover/checkbox:visible -rotate-12"
-                    />
-                  </div>
-
-                  <div className="text-xl text-black decoration-2 grow">
-                    Website design & dev
-                  </div>
-
-                  <div className="flex gap-3">
-                    <img className="rounded-full size-7" src="/profile.jpg" />
-                  </div>
-                </div>
-
-                {/* activity item (google - user) */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="bg-[#F84F39] rounded-lg size-6 flex items-center justify-center">
-                    <CheckIcon size={20} className="text-white -rotate-12" />
-                  </div>
-
-                  <div className="text-xl line-through decoration-orange-500 decoration-2 text-neutral-400 grow">
-                    Pricing strategy
-                  </div>
-
-                  <div className="flex gap-3">
-                    <img className="rounded-full size-7" src="/slack.png" />
-                    <img
-                      className="rounded-full size-7"
-                      src="/profile/65b30376cfc91a74393dc2c4_Jon_ts9dtj.webp"
-                    />
-                  </div>
-                </div>
-
-                {/* activity item (google - user) off */}
-                <div className="flex items-center justify-between gap-2">
-                  <div className="flex items-center justify-center border-2 rounded-lg size-6 group/checkbox border-neutral-400 text-neutral-400">
-                    <CheckIcon
-                      size={18}
-                      className="invisible group-hover/checkbox:visible -rotate-12"
-                    />
-                  </div>
-
-                  <div className="text-xl text-black decoration-2 grow">
-                    Prepare analytics
-                  </div>
-
-                  <div className="flex gap-3">
-                    <img
-                      className="rounded-full size-7"
-                      src="/profile/photo-1472099645785-5658abf4ff4e.avif"
-                    />
-                  </div>
-                </div>
-              </div>
             </div>
 
             {/* col 3 */}
-            <div className="hidden w-full h-full overflow-hidden bg-white lg:col-span-1 lg:block rounded-2xl">
-              <img
-                className="object-cover object-center w-full h-full"
-                src="/subsets/1244429.jpg"
-                alt=""
-              />
+            <div className="hidden col-span-1 col-start-4 p-3 bg-white rounded-2xl lg:block">
+              <div className="grid w-full grid-cols-2 gap-2">
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-16"></div>
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-16"></div>
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-16"></div>
+                <div className="w-full bg-[#F4F4F8] rounded-xl h-16"></div>
+              </div>
+
+              <div className="flex flex-col mt-10">
+                <div className="py-2 font-semibold text-center text-gray-800 hover:bg-[#F3F3F7]">
+                  Assignments
+                </div>
+                <div className="py-2 font-semibold text-center text-gray-800 hover:bg-[#F3F3F7]">
+                  Tasks
+                </div>
+                <div className="py-2 font-semibold text-center text-gray-800 hover:bg-[#F3F3F7]">
+                  Study Notes
+                </div>
+              </div>
             </div>
           </div>
         </div>
